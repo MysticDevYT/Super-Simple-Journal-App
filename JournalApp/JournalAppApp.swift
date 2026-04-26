cb7ed1527cf2ec6acfb962ae9257f73fcad6f673
Swastik Pal <swastikpal156@gmail.com>
2026-04-26 10:21:27 +0530

    Initial Commit

---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by swastik pal on 26/04/26.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
